666b56c9a4807d0d85f8691e6d31a21a590289b9
Joe Powers <54547147+ReadySetJoe@users.noreply.github.com>
2023-09-07 03:46:24 -0400

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,131 +1,133 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import Head from "next/head";
+
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>joepowers.dev</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="/">here.</a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
+          One day I'm gonna do some <code>coding</code> here
         </p>
+        <br />
+        <p className={styles.description}>For now, here are some projects:</p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+          <a
+            href="https://branch-out-gvl.herokuapp.com/"
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h3>branch.out</h3>
+            <p>A site for connecting you music taste, to a music place.</p>
+            <br />
+            <p> Which is very based.</p>
           </a>
 
           <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
+            href="https://main.d3oydlksdz52ep.amplifyapp.com/stats"
             className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
           >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
+            <h3>Slippi Stats</h3>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              A half-baked project based on parsing online melee replay files.
             </p>
+            <br />
+            <p>Niche like a quiche.</p>
           </a>
         </div>
       </main>
 
+      <p className={styles.description}>Non-coding things:</p>
+
+      <h3>Wolfgang Wallace</h3>
+      <div>
+        <iframe
+          src="https://www.youtube.com/embed/o2droJZTYYw?si=l2rr9O9dkhMo0tUb"
+          frameborder="0"
+          style={{
+            width: "100%",
+            height: "352px",
+            border: "0",
+            margin: "10px",
+          }}
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen
+        />
+        <iframe
+          style={{
+            width: "100%",
+            height: "352px",
+            border: "0",
+            margin: "10px",
+          }}
+          src="https://open.spotify.com/embed/artist/4m3Bhxg7otGtBF7xsfTQTV?utm_source=generator"
+          loading="lazy"
+        />
+      </div>
       <footer>
+        Made by Joe Powers{" "}
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://github.com/ReadySetJoe"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
+          <img
+            src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
+            alt="my github"
+            width="25"
+            height="25"
+          />
+        </a>
+        <a
+          href="https://www.twitch.tv/joepowers"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src="https://cdns.iconmonstr.com/wp-content/releases/preview/2016/240/iconmonstr-twitch-1.png"
+            alt="my twitch"
+            width="20"
+            height="20"
+          />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/joe-powers/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src="https://cdns.iconmonstr.com/wp-content/releases/preview/2012/240/iconmonstr-linkedin-3.png"
+            alt="my linkedin"
+            width="20"
+            height="20"
+          />
+        </a>
+        <a
+          href="mailto:joe.powers92@gmail.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src="https://static.vecteezy.com/system/resources/thumbnails/002/205/854/small/email-icon-free-vector.jpg"
+            alt="my email"
+            width="30"
+            height="30"
+          />
         </a>
       </footer>
-
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family:
-            Menlo,
-            Monaco,
-            Lucida Console,
-            Liberation Mono,
-            DejaVu Sans Mono,
-            Bitstream Vera Sans Mono,
-            Courier New,
-            monospace;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family:
-            -apple-system,
-            BlinkMacSystemFont,
-            Segoe UI,
-            Roboto,
-            Oxygen,
-            Ubuntu,
-            Cantarell,
-            Fira Sans,
-            Droid Sans,
-            Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
     </div>
   );
 }
